d90f3265fa2a8201a319cf04c898b53383bd239a
ibMH <54497010+ibMH@users.noreply.github.com>
2020-11-18 15:28:24 +0100

    Add files via upload

--- sync.sh
+++ sync.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-rsync --update --delete -avzhe ssh ~/catkin_tools_ws/src rosmatch@mur:~/catkin_tools_ws/
-rsync --update --delete -avzhe ssh ~/catkin_tools_ws/src rosmatch@miranda:~/catkin_tools_ws/
+echo "Sending to mur"
+rsync --delete -avzhe ssh ~/catkin_tools_ws/src rosmatch@mur:~/catkin_tools_ws/
+echo "Sending to miranda"
+rsync --delete -avzhe ssh ~/catkin_tools_ws/src rosmatch@miranda:~/catkin_tools_ws/
+#echo "Sending to master"
+#rsync --delete -avzhe ssh ~/catkin_tools_ws/src rosmatch@rosmaster:~/catkin_tools_ws/
 while inotifywait -r -e modify,create,delete,move  ~/catkin_tools_ws/src; do
-	rsync --update --delete -avzhe ssh ~/catkin_tools_ws/src rosmatch@mur:~/catkin_tools_ws/
-rsync --update --delete -avzhe ssh ~/catkin_tools_ws/src rosmatch@miranda:~/catkin_tools_ws/
+	echo "Sending to mur"
+	rsync --delete -avzhe ssh ~/catkin_tools_ws/src rosmatch@mur:~/catkin_tools_ws/
+	echo "Sending to miranda"
+	rsync --delete -avzhe ssh ~/catkin_tools_ws/src rosmatch@miranda:~/catkin_tools_ws/
+	#echo "Sending to master"
+	#rsync --delete -avzhe ssh ~/catkin_tools_ws/src rosmatch@rosmaster:~/catkin_tools_ws/	
 done
