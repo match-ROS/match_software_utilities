d9c4c507d698500ab49aae8b44e0bf99d1babc78
matchRos <47657974+matchRos@users.noreply.github.com>
2020-12-01 10:43:32 +0100

    Add files via upload

--- parse.m
+++ parse.m
@@ -1,7 +1,7 @@
 %% parse x and y positions from txt file
 
 % Name of the file
-filename = 'clicked_point2.txt';
+filename = 'large_trajectory.txt';
 A = textread(filename, '%s', 'whitespace', '');
 string = A{1,1};
 
@@ -31,4 +31,20 @@ end
 for i = 1:length(X(:,1))
     x_pose(i) = str2num(X(i,:));
     y_pose(i) = str2num(Y(i,:));
+end
+
+comp_even = ones(1,length(x_pose));
+comp_uneven = ones(1,length(x_pose));
+
+comp_even(1:2:end) = 0;
+comp_uneven(2:2:end) = 0;
+
+if sum(x_pose == comp_uneven) == 0
+    x_pose(1:2:end) = [];
+    y_pose(1:2:end) = [];
+elseif sum(x_pose == comp_even) == 0   
+    x_pose(1:2:end) = [];
+    y_pose(1:2:end) = [];
+else
+    warning("something went wrong")
 end
