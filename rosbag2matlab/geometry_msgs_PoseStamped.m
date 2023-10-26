39f9fee4cadf0aee2ce62b979ce07a1ef118f274
MiRgus <47657974+matchRos@users.noreply.github.com>
2023-10-26 15:46:16 +0200

    fixed typos

--- rosbag2matlab/geometry_msgs_PoseStamped.m
+++ rosbag2matlab/geometry_msgs_PoseStamped.m
@@ -1,16 +1,16 @@
 function structured_data = geometry_msgs_PoseStamped(data_struct)
 
-structured_data = []
+structured_data = [];
 
-for i = length(data_struct)
-   structured_data.pose.position.x = data_struct(i).Pose.Position.X;
-   structured_data.pose.position.y = data_struct(i).Pose.Position.Y;
-   structured_data.pose.position.z = data_struct(i).Pose.Position.Z;
+for i = 1:length(data_struct)
+   structured_data.pose.position.x(i) = data_struct(i).Pose.Position.X;
+   structured_data.pose.position.y(i) = data_struct(i).Pose.Position.Y;
+   structured_data.pose.position.z(i) = data_struct(i).Pose.Position.Z;
    
-   structured_data.pose.orientation.x = data_struct(i).Pose.Orientation.X;
-   structured_data.pose.orientation.y = data_struct(i).Pose.Orientation.Y;
-   structured_data.pose.orientation.z = data_struct(i).Pose.Orientation.Z;
-   structured_data.pose.orientation.w = data_struct(i).Pose.Orientation.W;
+   structured_data.pose.orientation.x(i) = data_struct(i).Pose.Orientation.X;
+   structured_data.pose.orientation.y(i) = data_struct(i).Pose.Orientation.Y;
+   structured_data.pose.orientation.z(i) = data_struct(i).Pose.Orientation.Z;
+   structured_data.pose.orientation.w(i) = data_struct(i).Pose.Orientation.W;
    
 end
 
--- rosbag2matlab/geometry_msgs_PoseWithCovarianceStamped.m
+++ rosbag2matlab/geometry_msgs_PoseWithCovarianceStamped.m
@@ -1,16 +1,16 @@
 function structured_data = geometry_msgs_PoseWithCovarianceStamped(data_struct)
 
-structured_data = []
+structured_data = [];
 
-for i = length(data_struct)
-   structured_data.pose.pose.position.x = data_struct(i).Pose.Pose.Position.X;
-   structured_data.pose.pose.position.y = data_struct(i).Pose.Pose.Position.Y;
-   structured_data.pose.pose.position.z = data_struct(i).Pose.Pose.Position.Z;
+for i = 1:length(data_struct)
+   structured_data.pose.pose.position.x(i) = data_struct(i).Pose.Pose.Position.X;
+   structured_data.pose.pose.position.y(i) = data_struct(i).Pose.Pose.Position.Y;
+   structured_data.pose.pose.position.z(i) = data_struct(i).Pose.Pose.Position.Z;
    
-   structured_data.pose.pose.orientation.x = data_struct(i).Pose.Pose.Orientation.X;
-   structured_data.pose.pose.orientation.y = data_struct(i).Pose.Pose.Orientation.Y;
-   structured_data.pose.pose.orientation.z = data_struct(i).Pose.Pose.Orientation.Z;
-   structured_data.pose.pose.orientation.w = data_struct(i).Pose.Pose.Orientation.W;
+   structured_data.pose.pose.orientation.x(i) = data_struct(i).Pose.Pose.Orientation.X;
+   structured_data.pose.pose.orientation.y(i) = data_struct(i).Pose.Pose.Orientation.Y;
+   structured_data.pose.pose.orientation.z(i) = data_struct(i).Pose.Pose.Orientation.Z;
+   structured_data.pose.pose.orientation.w(i) = data_struct(i).Pose.Pose.Orientation.W;
    
 end
 
