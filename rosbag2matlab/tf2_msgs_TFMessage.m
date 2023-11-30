21b5acb142d72297319687dee82c8dfeff7104c8
MiRgus <47657974+matchRos@users.noreply.github.com>
2023-11-30 11:36:37 +0100

    Add files via upload

--- rosbag2matlab/tf2_msgs_TFMessage.m
+++ rosbag2matlab/tf2_msgs_TFMessage.m
@@ -18,9 +18,14 @@ for i = 1:length(data_struct)
                 structured_data.(frame_id).transform.translation.z(end +1) = data_struct(i).Transforms(j).Transform.Translation.Z;
                 
                 structured_data.(frame_id).transform.rotation.x(end +1) = data_struct(i).Transforms(j).Transform.Rotation.X;
-                structured_data.(frame_id).transform.rotation.x(end +1) = data_struct(i).Transforms(j).Transform.Rotation.X;
-                structured_data.(frame_id).transform.rotation.x(end +1) = data_struct(i).Transforms(j).Transform.Rotation.X;
-                structured_data.(frame_id).transform.rotation.x(end +1) = data_struct(i).Transforms(j).Transform.Rotation.X;
+                structured_data.(frame_id).transform.rotation.y(end +1) = data_struct(i).Transforms(j).Transform.Rotation.Y;
+                structured_data.(frame_id).transform.rotation.z(end +1) = data_struct(i).Transforms(j).Transform.Rotation.Z;
+                structured_data.(frame_id).transform.rotation.w(end +1) = data_struct(i).Transforms(j).Transform.Rotation.W;
+                eulZYX = quat2eul([data_struct(i).Transforms(j).Transform.Rotation.X,data_struct(i).Transforms(j).Transform.Rotation.Y, ...
+                    data_struct(i).Transforms(j).Transform.Rotation.Z,data_struct(i).Transforms(j).Transform.Rotation.W]);
+                structured_data.(frame_id).transform.rotation.eulX(end +1) = eulZYX(1);
+                structured_data.(frame_id).transform.rotation.eulY(end +1) = eulZYX(2);
+                structured_data.(frame_id).transform.rotation.eulZ(end +1) = eulZYX(3);
                 
             else
                 structured_data.(frame_id).transform.translation.x(1) = data_struct(i).Transforms(j).Transform.Translation.X;
@@ -31,12 +36,22 @@ for i = 1:length(data_struct)
                 structured_data.(frame_id).transform.rotation.y(1) = data_struct(i).Transforms(j).Transform.Rotation.Y;
                 structured_data.(frame_id).transform.rotation.z(1) = data_struct(i).Transforms(j).Transform.Rotation.Z;
                 structured_data.(frame_id).transform.rotation.w(1) = data_struct(i).Transforms(j).Transform.Rotation.W;
+                eulZYX = quat2eul([data_struct(i).Transforms(j).Transform.Rotation.X,data_struct(i).Transforms(j).Transform.Rotation.Y, ...
+                    data_struct(i).Transforms(j).Transform.Rotation.Z,data_struct(i).Transforms(j).Transform.Rotation.W]);
+                structured_data.(frame_id).transform.rotation.eulX(1) = eulZYX(1);
+                structured_data.(frame_id).transform.rotation.eulY(1) = eulZYX(2);
+                structured_data.(frame_id).transform.rotation.eulZ(1) = eulZYX(3);
                 
             end
             
 
         end
+        
+        structured_data.(frame_id).transform.rotation.eulXYZ = [unwrap(structured_data.(frame_id).transform.rotation.eulX); ...
+        unwrap(structured_data.(frame_id).transform.rotation.eulY); unwrap(structured_data.(frame_id).transform.rotation.eulZ)];
        
 end
+
+
 
 end
